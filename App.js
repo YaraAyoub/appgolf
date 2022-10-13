74b5e180c0a5503176f34e3071a664e29227351a
Yara <yara.ayoub@edu.ece.fr>
2022-10-13 12:29:00 +0200

    add img

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, AppRegistry, Image } from 'react-native';
+ 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./img/fondtest.png')} style={{height:800, width:370, resizeMode:'stretch',}}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },
